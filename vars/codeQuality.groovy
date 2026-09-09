160d38a2efcb4b0348dd5d0501c1866fd53c6331
Yoav Barel <yoav.barel1@gmail.com>
2026-09-09 15:23:31 +0300

    update shared library18

--- vars/codeQuality.groovy
+++ vars/codeQuality.groovy
@@ -1,11 +1,11 @@
 def sonarCreateProject(String projectKey) {
-        withSonarQubeEnv('SonarQubeScanner') {
-            sh """
-                curl -s -u ${env.SONAR_TOKEN}: \
-               -X POST "${env.SONAR_HOST_URL}/api/projects/create" \
-               -d "project=${projectKey}&name=${projectKey}"
-            """
-        }
+    withSonarQubeEnv('SonarQubeScanner') {
+        sh """
+            curl -s -u ${env.SONAR_AUTH_TOKEN}: \
+            -X POST "${env.SONAR_HOST_URL}/api/projects/create" \
+            -d "project=${projectKey}&name=${projectKey}"
+        """
+    }
 }
 
 def sonarLocalScan() {
